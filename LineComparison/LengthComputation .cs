39f05172d864e038f797d866bc886ca991ddd56c
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-09-30 21:00:36 +0530

    [mrunal] Add. Line computation using interface approach

---
+++ LineComparison/LengthComputation .cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    public interface ILineComputation
+    {
+        public void CompareLengths();
+    }
+    class LengthComputation : ILineComputation
+    {
+        public double len1;
+        public double len2;
+
+        public void LengthCalculation(double x1, double x2, double y1, double y2, int index)
+        {
+
+            double length;
+
+            double val1 = Math.Pow((x2 - x1), 2);
+            double val2 = Math.Pow((y2 - y1), 2);
+            length = Math.Sqrt(val1 + val2);
+
+            if (index == 1)
+            {
+                len1 = length;
+
+            }
+            else
+            {
+                len2 = length;
+            }
+            Console.WriteLine("Length of line " + index + " : " + length + "\n");
+
+        }
+
+        public void CompareLengths()
+        {
+            var val = len1.CompareTo(len2);
+            if (val == 0)
+            {
+                Console.WriteLine("The two lines are equal");
+            }
+            else if (val == 1)
+            {
+                Console.WriteLine("Line 1 is greater than Line 2");
+            }
+            else if (val == -1)
+            {
+                Console.WriteLine("Line 1 is less than Line 2");
+            }
+        }
+    }
+}
+
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
+﻿
+using System;
 
 namespace LineComparison
 {
     class Program
     {
-        public static double len1;
-        public static double len2;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
 
-            for (int i=1; i<=2; i++)
+            LengthComputation lenComputation = new LengthComputation();
+
+            for (int i = 1; i <= 2; i++)
             {
                 Console.WriteLine("Enter points of line " + i + " :");
                 Console.WriteLine("Enter coordinates of point 1 (x1 and y1)");
@@ -22,51 +23,11 @@ namespace LineComparison
                 var x2 = Convert.ToDouble(Console.ReadLine());
                 var y2 = Convert.ToDouble(Console.ReadLine());
 
-                LengthCalculation(x1, x2, y1, y2, i);
+                lenComputation.LengthCalculation(x1, x2, y1, y2, i);
 
             }
-            CompareLengths();
+            lenComputation.CompareLengths();
 
-        }
-
-        // Use Case 3
-        static void LengthCalculation(double x1, double x2, double y1, double y2, int index)
-        {
-            
-            double length;
-
-            double val1 = Math.Pow((x2 - x1), 2);
-            double val2 = Math.Pow((y2 - y1), 2);
-            length = Math.Sqrt(val1 + val2);
-
-            if (index == 1)
-            {
-                len1 = length;
-                
-            }
-            else
-            {
-                len2 = length;
-            }
-            Console.WriteLine("Length of line " + index + " : " + length + "\n");
-
-        }
-
-        static void CompareLengths()
-        {
-            var val = len1.CompareTo(len2);
-            if (val == 0)
-            {
-                Console.WriteLine("The two lines are equal");
-            } 
-            else if (val == 1)
-            {
-                Console.WriteLine("Line 1 is greater than Line 2");
-            }
-            else if (val == -1)
-            {
-                Console.WriteLine("Line 1 is less than Line 2");
-            }
         }
     }
 }
